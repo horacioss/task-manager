bf3c5a10c3b50d631f2df30392b41e48b949a60d
Horacio Macias <horacioss2013@gmail.com>
2021-08-22 14:09:13 -0400

    Delete jenkins directory

--- jenkins/scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-echo 'The following "npm" command tests that your Task Manager App'
-echo 'application renders satisfactorily. This command actually invokes the test'
-echo 'runner Jest (https://facebook.github.io/jest/).'
-set -x
-yarn test
